121d463bc50b1f67bc0777ffe41a85a32ee8968c
Denys Popov <besisland@besisland.name>
2011-08-04 17:52:08 +0300

    Rendering exit point.

--- Labyrinth/Game.cs
+++ Labyrinth/Game.cs
@@ -27,12 +27,12 @@ namespace Labyrinth {
         private float wallsHeight = 0.7f;
 
         private int textureWall;
-		
+
 		private float torchLight = 0.15f;
 		private float torchLightMaxChange = 0.01f;
 		private float torchLightMin = 0.05f;
 		private float torchLightMax = 0.3f;
-		
+
         public Game()
             : base(800, 600, GraphicsMode.Default, "OpenGL Test #1") {
             VSync = VSyncMode.On;
@@ -43,16 +43,18 @@ namespace Labyrinth {
             base.OnLoad(e);
 
             GL.ClearColor(Color4.Black);
+            GL.Color4(Color4.Transparent);
             GL.Enable(EnableCap.DepthTest);
             GL.Enable(EnableCap.Texture2D);
             GL.Enable(EnableCap.Lighting);
             GL.Enable(EnableCap.Light0);
+            GL.Enable(EnableCap.Light1);
 
             textureWall = LoadTexture("../../textures/wall.png");
 
-            map = new Map(25, 25); // TODO parametrize
+            map = new Map(10, 10); // TODO parametrize
 
-            playerPosition = new Vector3(map.StartPosition.X + 0.5f, map.StartPosition.Y + 0.5f, 0.2f);
+            playerPosition = new Vector3(map.StartPosition.X + 0.5f, map.StartPosition.Y + 0.5f, 0.5f);
             playerAngle = 0;
         }
 
@@ -60,7 +62,7 @@ namespace Labyrinth {
             base.OnResize(e);
 
             GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
-			
+
             var projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 0.00001f, Math.Max(map.Width, map.Height) * 2f);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref projection);
@@ -125,17 +127,26 @@ namespace Labyrinth {
 
             var torchPosition = new Vector4(playerPosition);
             torchPosition.W = 1;
-			
+
 			torchLight += (rand.Next(-100, +100) / 100f) * torchLightMaxChange;
 			torchLight = Math.Max(torchLight, torchLightMin);
 			torchLight = Math.Min(torchLight, torchLightMax);
-			
+
             GL.Light(LightName.Light0, LightParameter.Position, torchPosition);
             GL.Light(LightName.Light0, LightParameter.ConstantAttenuation, torchLight);
             GL.Light(LightName.Light0, LightParameter.Diffuse, Color4.SaddleBrown);
 
+            GL.Light(LightName.Light1, LightParameter.Position, new Vector4(map.FinishPosition.X + 0.5f, map.FinishPosition.Y + 0.5f, wallsHeight * 2, 1));
+            GL.Light(LightName.Light1, LightParameter.Diffuse, Color4.White);
+            GL.Light(LightName.Light1, LightParameter.Specular, Color4.White);
+            GL.Light(LightName.Light1, LightParameter.ConstantAttenuation, 0.1f);
+            GL.Light(LightName.Light1, LightParameter.SpotDirection, new Vector4(0, 0, -1, 0));
+            GL.Light(LightName.Light1, LightParameter.SpotCutoff, 60);
+
             for (var x = 0; x < map.Width; x++) {
                 for (var y = 0; y < map.Height; y++) {
+                    var position = new Vector2(x, y);
+
                     if (Map.CellType.Empty == map.GetCell(x, y)) {
                         if (Map.CellType.Wall == map.GetCell(x, y + 1)) {
                             RenderWall(new Vector2(x, y + 1), new Vector2(x + 1, y + 1));
@@ -149,8 +160,13 @@ namespace Labyrinth {
                         if (Map.CellType.Wall == map.GetCell(x + 1, y)) {
                             RenderWall(new Vector2(x + 1, y + 1), new Vector2(x + 1, y));
                         }
-						
-						RenderFloorAndCeiling(new Vector2(x, y));
+
+                        RenderFloor(position);
+                        if (map.FinishPosition.Equals(position)) {
+                            RenderExit(position);
+                        } else {
+                            RenderCeiling(position);
+                        }
                     }
                 }
             }
@@ -170,24 +186,26 @@ namespace Labyrinth {
 
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-            
+
             return texture;
         }
 
-        private void RenderWall(Vector2 A, Vector2 B) {
-            GL.Color4(Color4.Transparent);
+        private void RenderWall(Vector2 A, Vector2 B, float z) {
             GL.BindTexture(TextureTarget.Texture2D, textureWall);
 
             GL.Begin(BeginMode.Quads);
-            GL.TexCoord2(0, 0); GL.Vertex3(A.X, A.Y, wallsHeight);
-            GL.TexCoord2(1, 0); GL.Vertex3(B.X, B.Y, wallsHeight);
-            GL.TexCoord2(1, wallsHeight); GL.Vertex3(B.X, B.Y, 0);
-            GL.TexCoord2(0, wallsHeight); GL.Vertex3(A.X, A.Y, 0); 
+            GL.TexCoord2(0, 0); GL.Vertex3(A.X, A.Y, z + wallsHeight);
+            GL.TexCoord2(1, 0); GL.Vertex3(B.X, B.Y, z + wallsHeight);
+            GL.TexCoord2(1, wallsHeight); GL.Vertex3(B.X, B.Y, z);
+            GL.TexCoord2(0, wallsHeight); GL.Vertex3(A.X, A.Y, z);
             GL.End();
         }
 
-        private void RenderFloorAndCeiling(Vector2 position) {
-            GL.Color4(Color4.Transparent);
+        private void RenderWall(Vector2 A, Vector2 B) {
+            RenderWall(A, B, 0);
+        }
+
+        private void RenderFloor(Vector2 position) {
             GL.BindTexture(TextureTarget.Texture2D, textureWall);
 
             GL.Begin(BeginMode.Quads);
@@ -196,12 +214,38 @@ namespace Labyrinth {
             GL.TexCoord2(1, 0); GL.Vertex3(position.X + 1, position.Y + 1, 0);
             GL.TexCoord2(0, 0); GL.Vertex3(position.X, position.Y + 1, 0);
             GL.End();
+        }
+
+        private void RenderCeiling(Vector2 position) {
+            GL.BindTexture(TextureTarget.Texture2D, textureWall);
 
             GL.Begin(BeginMode.Quads);
             GL.TexCoord2(0, 0); GL.Vertex3(position.X, position.Y, wallsHeight);
             GL.TexCoord2(1, 0); GL.Vertex3(position.X + 1, position.Y, wallsHeight);
             GL.TexCoord2(1, 1); GL.Vertex3(position.X + 1, position.Y + 1, wallsHeight);
             GL.TexCoord2(0, 1); GL.Vertex3(position.X, position.Y + 1, wallsHeight);
+            GL.End();
+        }
+
+        private void RenderExit(Vector2 position) {
+            RenderWall(new Vector2(position.X, position.Y + 1), new Vector2(position.X + 1, position.Y + 1), wallsHeight);
+            RenderWall(new Vector2(position.X + 1, position.Y), new Vector2(position.X, position.Y), wallsHeight);
+            RenderWall(new Vector2(position.X, position.Y), new Vector2(position.X, position.Y + 1), wallsHeight);
+            RenderWall(new Vector2(position.X + 1, position.Y + 1), new Vector2(position.X + 1, position.Y), wallsHeight);
+
+            var ropeWidth = 0.01;
+
+            GL.Begin(BeginMode.QuadStrip);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 - ropeWidth, 0.3);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 - ropeWidth, wallsHeight * 2);
+            GL.Vertex3(position.X + 0.5 + ropeWidth, position.Y + 0.5 - ropeWidth, 0.3);
+            GL.Vertex3(position.X + 0.5 + ropeWidth, position.Y + 0.5 - ropeWidth, wallsHeight * 2);
+            GL.Vertex3(position.X + 0.5 + ropeWidth, position.Y + 0.5 + ropeWidth, 0.3);
+            GL.Vertex3(position.X + 0.5 + ropeWidth, position.Y + 0.5 + ropeWidth, wallsHeight * 2);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 + ropeWidth, 0.3);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 + ropeWidth, wallsHeight * 2);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 - ropeWidth, 0.3);
+            GL.Vertex3(position.X + 0.5 - ropeWidth, position.Y + 0.5 - ropeWidth, wallsHeight * 2);
             GL.End();
         }
     }
