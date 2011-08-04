5b67cdc69a5048f5d1858e7ceb47f070f4a9f248
Denys Popov <besisland@besisland.name>
2011-08-05 00:48:55 +0300

    abstract class GameWindowLayer

---
+++ Labyrinth/GameWindowLayer.cs
@@ -0,0 +1,8 @@
+﻿using OpenTK;
+
+namespace Labyrinth {
+    abstract class GameWindowLayer {
+        abstract public void OnUpdateFrame(GameWindow window);
+        abstract public void OnRenderFrame(GameWindow window);
+    }
+}
