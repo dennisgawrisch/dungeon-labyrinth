2980c43e5de2fc0268cd37ff74963bb88e2988ab
Denys Popov <besisland@besisland.name>
2011-11-10 22:49:25 +0200

    Rendering text in menu.

--- Labyrinth/Gui/Menu/Button.cs
+++ Labyrinth/Gui/Menu/Button.cs
@@ -1,44 +1,12 @@
 using System;
-using System.Collections.Generic;
-using OpenTK;
-using OpenTK.Graphics;
-using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
-using Labyrinth.Gui;
 
 namespace Labyrinth.Gui.Menu {
-    class Button : Element {
-        public string Label;
-        public bool Enabled = true;
+    class Button : Text {
+        public bool Enabled = true; // TODO respect while rendering
 
-        public Button(string Label) {
-            this.Label = Label;
-        }
-
-        protected override int GetRequiredWidth() {
-            return Label.Length * 50; // TODO
-        }
-
-        protected override int GetRequiredHeight() {
-            return 50; // TODO
-        }
-
-        public override void Render() {
-            GL.PushAttrib(AttribMask.AllAttribBits);
-
-            GL.Color4(Color4.ForestGreen);
-            GL.Begin(BeginMode.Quads);
-            GL.Vertex2(Left.Value, Top.Value);
-            GL.Vertex2(Right.Value, Top.Value);
-            GL.Vertex2(Right.Value, Bottom.Value);
-            GL.Vertex2(Left.Value, Bottom.Value);
-            GL.End();
-
-            GL.PopAttrib();
-
-            // TODO
-
-            // TODO respect Enabled
+        public Button(string Label) 
+            : base(Label) {
         }
 
         public event EventHandler<EventArgs> Enter;
@@ -50,7 +18,7 @@ namespace Labyrinth.Gui.Menu {
         }
 
         public override void OnKeyPress(Key K) {
-            if (Enabled && K.Equals(Key.Enter)) {
+            if (Enabled && (K.Equals(Key.Enter) || K.Equals(Key.KeypadEnter))) {
                 OnEnter(EventArgs.Empty);
             }
         }
