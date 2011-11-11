f0e3a794f03f950705d7a6bc7831d15553d8009c
Denys Popov <denys@denyspopov.biz>
2011-11-11 19:54:53 +0200

    Calculating bounds for text GUI elements, word-wrapping enabled.

--- Labyrinth/Gui/Text.cs
+++ Labyrinth/Gui/Text.cs
@@ -13,8 +13,10 @@ namespace Labyrinth.Gui {
     class Text : Element {
         private string LabelValue;
         private Font FontValue;
+        private bool NeedToMeasureRequiredWidth, NeedToMeasureRequiredHeight, NeedToRenderTexture;
+        private int CachedRequiredWidth, CachedRequiredHeight, CachedRequiredHeightForWidth;
         private int Texture;
-        private int RequiredWidth, RequiredHeight;
+        private int RenderedTextureForWidth, RenderedTextureForHeight;
 
         public string Label {
             get {
@@ -23,7 +25,9 @@ namespace Labyrinth.Gui {
 
             set {
                 LabelValue = value;
-                RenderTexture();
+                NeedToMeasureRequiredWidth = true;
+                NeedToMeasureRequiredHeight = true;
+                NeedToRenderTexture = true;
             }
         }
 
@@ -34,50 +38,48 @@ namespace Labyrinth.Gui {
 
             set {
                 FontValue = value;
-                RenderTexture();
+                NeedToMeasureRequiredWidth = true;
+                NeedToMeasureRequiredHeight = true;
+                NeedToRenderTexture = true;
             }
         }
 
         public Text(string Label) {
-            LabelValue = Label;
-            FontValue = new Font(new FontFamily(GenericFontFamilies.SansSerif), 50, GraphicsUnit.Pixel);
+            this.Label = Label;
+            Font = new Font(new FontFamily(GenericFontFamilies.SansSerif), 50, GraphicsUnit.Pixel);
             Texture = GL.GenTexture();
-            RenderTexture();
         }
 
         protected void RenderTexture() {
-            using (var Bitmap = new Bitmap(1, 1)) {
-                using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
-                    var Measures = Graphics.MeasureString(Label, Font);
-                    RequiredWidth = (int)Math.Ceiling(Measures.Width);
-                    RequiredHeight = (int)Math.Ceiling(Measures.Height);
-                }
-            }
-
             GL.BindTexture(TextureTarget.Texture2D, Texture);
-            using (var Bitmap = new Bitmap(GetRequiredWidth(), GetRequiredHeight())) {
-                var Rectangle = new Rectangle(0, 0, Bitmap.Width, Bitmap.Height);
-                using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
-                    Graphics.Clear(Color.Transparent);
-                    Graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
-                    Graphics.DrawString(Label, Font, Brushes.White, Rectangle);
-
-                    var BitmapData = Bitmap.LockBits(Rectangle, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                    GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, BitmapData.Width, BitmapData.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, BitmapData.Scan0);
-                    Bitmap.UnlockBits(BitmapData);
-
-                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-                    GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-                }
-            }
         }
 
         protected override int GetRequiredWidth() {
-            return RequiredWidth;
+            if (NeedToMeasureRequiredWidth) {
+                using (var Bitmap = new Bitmap(1, 1)) {
+                    using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
+                        var Measures = Graphics.MeasureString(Label, Font);
+                        CachedRequiredWidth = (int)Math.Ceiling(Measures.Width);
+                    }
+                }
+                NeedToMeasureRequiredWidth = false;
+                NeedToMeasureRequiredHeight = true;
+            }
+            return CachedRequiredWidth;
         }
 
         protected override int GetRequiredHeight() {
-            return RequiredHeight;
+            if (NeedToMeasureRequiredHeight || (CachedRequiredHeightForWidth != Width.Value)) {
+                using (var Bitmap = new Bitmap(1, 1)) {
+                    using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
+                        var Measures = Graphics.MeasureString(Label, Font, Width.Value);
+                        CachedRequiredHeight = (int)Math.Ceiling(Measures.Height);
+                    }
+                }
+                NeedToMeasureRequiredHeight = false;
+                CachedRequiredHeightForWidth = Width.Value;
+            }
+            return CachedRequiredHeight;
         }
 
         public override void Render() {
@@ -89,13 +91,32 @@ namespace Labyrinth.Gui {
             GL.Enable(EnableCap.Texture2D);
             GL.BindTexture(TextureTarget.Texture2D, Texture);
 
-            var TextWidth = GetRequiredWidth(); // TODO this is hack, it ignores the bounds (Left, Right, Width etc.)
+            if (NeedToRenderTexture || (RenderedTextureForWidth != Width.Value) || (RenderedTextureForHeight != Height.Value)) {
+                using (var Bitmap = new Bitmap(Width.Value, Height.Value)) {
+                    var Rectangle = new Rectangle(0, 0, Bitmap.Width, Bitmap.Height);
+                    using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
+                        Graphics.Clear(Color.Transparent);
+                        Graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+                        Graphics.DrawString(Label, Font, Brushes.White, Rectangle);
+
+                        var BitmapData = Bitmap.LockBits(Rectangle, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, BitmapData.Width, BitmapData.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, BitmapData.Scan0);
+                        Bitmap.UnlockBits(BitmapData);
+
+                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+                    }
+                }
+                NeedToRenderTexture = false;
+                RenderedTextureForWidth = Width.Value;
+                RenderedTextureForHeight = Height.Value;
+            }
 
             GL.Color4(Color4.Black);
             GL.Begin(BeginMode.Quads);
             GL.TexCoord2(0, 0); GL.Vertex2(Left.Value, Top.Value);
-            GL.TexCoord2(1, 0); GL.Vertex2(Right.Value - Width.Value + TextWidth, Top.Value);
-            GL.TexCoord2(1, 1); GL.Vertex2(Right.Value - Width.Value + TextWidth, Bottom.Value);
+            GL.TexCoord2(1, 0); GL.Vertex2(Right.Value, Top.Value);
+            GL.TexCoord2(1, 1); GL.Vertex2(Right.Value, Bottom.Value);
             GL.TexCoord2(0, 1); GL.Vertex2(Left.Value, Bottom.Value);
             GL.End();
 
