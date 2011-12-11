75c37b47a5e047182b38f096786e9c01a8e1822c
Denys Popov <denys@denyspopov.biz>
2011-12-11 19:21:16 +0200

    Implemented code from ImpressiveSolids.TextRenderer into Gui.Text.

--- Labyrinth/Gui/Text.cs
+++ Labyrinth/Gui/Text.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Drawing.Text;
-using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
@@ -15,7 +10,7 @@ namespace Labyrinth.Gui {
         private Font FontValue;
         private bool NeedToMeasureRequiredWidth, NeedToMeasureRequiredHeight, NeedToRenderTexture;
         private int CachedRequiredWidth, CachedRequiredHeight, CachedRequiredHeightForWidth;
-        private int Texture;
+        private Texture Texture;
         private int RenderedTextureForWidth, RenderedTextureForHeight;
 
         public string Label {
@@ -44,14 +39,11 @@ namespace Labyrinth.Gui {
             }
         }
 
+        public Color4 Color = Color4.Black;
+
         public Text(string Label) {
             this.Label = Label;
             Font = new Font(new FontFamily(GenericFontFamilies.SansSerif), 30, GraphicsUnit.Pixel);
-            Texture = GL.GenTexture();
-        }
-
-        protected void RenderTexture() {
-            GL.BindTexture(TextureTarget.Texture2D, Texture);
         }
 
         protected override int GetRequiredWidth() {
@@ -83,28 +75,29 @@ namespace Labyrinth.Gui {
         }
 
         public override void Render() {
+            if ((null == Label) || ("" == Label)) {
+                return;
+            }
+
             GL.PushAttrib(AttribMask.AllAttribBits);
 
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
 
             GL.Enable(EnableCap.Texture2D);
-            GL.BindTexture(TextureTarget.Texture2D, Texture);
 
             if (NeedToRenderTexture || (RenderedTextureForWidth != Width.Value) || (RenderedTextureForHeight != Height.Value)) {
                 using (var Bitmap = new Bitmap(Width.Value, Height.Value)) {
                     var Rectangle = new Rectangle(0, 0, Bitmap.Width, Bitmap.Height);
                     using (Graphics Graphics = Graphics.FromImage(Bitmap)) {
-                        Graphics.Clear(Color.Transparent);
+                        Graphics.Clear(System.Drawing.Color.Transparent);
                         Graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
                         Graphics.DrawString(Label, Font, Brushes.White, Rectangle);
 
-                        var BitmapData = Bitmap.LockBits(Rectangle, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-                        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, BitmapData.Width, BitmapData.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, BitmapData.Scan0);
-                        Bitmap.UnlockBits(BitmapData);
-
-                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-                        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+                        if (null != Texture) {
+                            Texture.Dispose();
+                        }
+                        Texture = new Texture(Bitmap);
                     }
                 }
                 NeedToRenderTexture = false;
@@ -112,16 +105,25 @@ namespace Labyrinth.Gui {
                 RenderedTextureForHeight = Height.Value;
             }
 
-            GL.Color4(Color4.Black);
-            GL.Begin(BeginMode.Quads);
-            GL.TexCoord2(0, 0); GL.Vertex2(Left.Value, Top.Value);
-            GL.TexCoord2(1, 0); GL.Vertex2(Right.Value, Top.Value);
-            GL.TexCoord2(1, 1); GL.Vertex2(Right.Value, Bottom.Value);
-            GL.TexCoord2(0, 1); GL.Vertex2(Left.Value, Bottom.Value);
-            GL.End();
+            Texture.Bind();
 
-            GL.Disable(EnableCap.Blend);
-            GL.Disable(EnableCap.Texture2D);
+            GL.Color4(Color);
+
+            GL.Begin(BeginMode.Quads);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(Left.Value, Top.Value);
+
+            GL.TexCoord2((float)Texture.Width / Texture.PotWidth, 0);
+            GL.Vertex2(Right.Value, Top.Value);
+
+            GL.TexCoord2((float)Texture.Width / Texture.PotWidth, (float)Texture.Height / Texture.PotHeight);
+            GL.Vertex2(Right.Value, Bottom.Value);
+
+            GL.TexCoord2(0, (float)Texture.Height / Texture.PotHeight);
+            GL.Vertex2(Left.Value, Bottom.Value);
+
+            GL.End();
 
             GL.PopAttrib();
         }
